f7db44bb3a0f60e319323a43dacea8e4fffdb656
Kenny Tran <kenny.tran138@gmail.com>
2021-02-15 23:13:04 -0500

    added attributes as attachments to DOM elements

--- calculator.js
+++ calculator.js
@@ -9,38 +9,118 @@ $(document).ready(function () {
   var $screenText = $('<p class="screen"></p>');
 
   // a container for the top three buttons with class operator
-  var $aux = $('<div class="button-container aux"></div>');
+  var $aux = $('<div></div>')
+    .attr({
+      class: 'button-container aux'
+    });
   // this will contain three buttons
-  var $clear = $('<button class="btn operator" id="clear"></button>');
-  var $sign = $('<button class="btn operator" id="sign"></button>');
-  var $percent = $('<button class="btn operator" id="percent"></button>');
+  var $clear = $('<button class="btn operator" id="clear"></button>')
+    .attr({
+      class: 'btn operator',
+      id: 'clear'
+    });
+  var $sign = $('<button></button>')
+    .attr({
+      class: 'btn operator',
+      id: 'sign'
+    });
+  var $percent = $('<button></button>')
+    .attr({
+      class: 'btn operator',
+      id: 'percent'
+    });
 
   // a container for the four operators on the right side clas operator
-  var $operators = $('<div class="button-container operators"></div>')
+  var $operators = $('<div></div>')
     .attr({
       class: 'button-container operators'
     });
   // this will contain 5 buttons
-  var $division = $('<button class="btn operator" id="division"></button>');
-  var $multiplication = $('<button class="btn operator" id="multiplication"></button>');
-  var $subtraction = $('<button class="btn operator" id="subtraction"></button>');
-  var $addition = $('<button class="btn operator" id="addition"></button>');
-  var $equals = $('<button class="btn operator" id="equals"></button>');
+  var $division = $('<button></button>')
+    .attr({
+      class: 'btn operator',
+      id: 'division'
+    });
+  var $multiplication = $('<button></button>')
+    .attr({
+      class: 'btn operator',
+      id: 'multiplication'
+    });
+  var $subtraction = $('<button></button>')
+    .attr({
+      class: 'btn operator',
+      id: 'subtraction'
+    });
+  var $addition = $('<button></button>')
+    .attr({
+      class: 'btn operator',
+      id: 'addition'
+    });
+  var $equals = $('<button></button>')
+    .attr({
+      class: 'btn operator',
+      id: 'equals'
+    });
 
   // a container for all numbers an decimal
-  var $numbers = $('<div class="button-container numbers"></div>');
+  var $numbers = $('<div></div>')
+    .attr('class', 'button-container numbers');
   // this will contain 11 buttons
-  var $seven = $('<button class="btn number" id="seven"></button>');
-  var $eight = $('<button class="btn number" id="eight"></button>');
-  var $nine = $('<button class="btn number" id="nine"></button>');
-  var $four = $('<button class="btn number" id="four"></button>');
-  var $five = $('<button class="btn number" id="five"></button>');
-  var $six = $('<button class="btn number" id="six"></button>');
-  var $one = $('<button class="btn number" id="one"></button>');
-  var $two = $('<button class="btn number" id="two"></button>');
-  var $three = $('<button class="btn number" id="three"></button>');
-  var $zero = $('<button class="btn number" id="zero"></button>');
-  var $decimal = $('<button class="btn number" id="decimal"></button>');
+  var $seven = $('<button></button>')
+    .attr({
+      class: 'btn number',
+      id: 'seven'
+    });
+  var $eight = $('<button ></button>')
+    .attr({
+      class: 'btn number',
+      id: 'eight'
+    });
+  var $nine = $('<button></button>')
+    .attr({
+      class: 'btn number',
+      id: 'eight'
+    });
+  var $four = $('<button></button>')
+    .attr({
+      class: 'btn number',
+      id: 'four'
+    });
+  var $five = $('<button></button>')
+    .attr({
+      class: 'btn number',
+      id: 'five'
+    });
+  var $six = $('<button></button>')
+    .attr({
+      class: 'btn number',
+      id: 'six'
+    });
+  var $one = $('<button></button>')
+    .attr({
+      class: 'btn number',
+      id: 'one'
+    });
+  var $two = $('<button></button>')
+    .attr({
+      class: 'btn number',
+      id: 'two'
+    });
+  var $three = $('<button></button>')
+    .attr({
+      class: 'btn number',
+      id: 'three'
+    });
+  var $zero = $('<button></button>')
+    .attr({
+      class: 'btn number',
+      id: 'zero'
+    });
+  var $decimal = $('<button></button>')
+    .attr({
+      class: 'btn number',
+      id: 'decimal'
+    });
 
   // helper functions
 
