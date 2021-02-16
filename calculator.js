e4b0f9997871c617e80c75cc1e52afaa5064280f
Kenny Tran <kenny.tran138@gmail.com>
2021-02-15 23:31:50 -0500

    deleted redundant class names already covered by html tag type

--- calculator.js
+++ calculator.js
@@ -13,22 +13,22 @@ $(document).ready(function () {
   // a container for the top three buttons with class operator
   var $aux = $('<div></div>')
     .attr({
-      class: 'button-container aux'
+      class: 'container aux'
     });
   // this will contain three buttons
-  var $clear = $('<button class="btn operator" id="clear"></button>')
+  var $clear = $('<button></button>')
     .attr({
-      class: 'btn operator',
+      class: 'operator',
       id: 'clear'
     });
   var $sign = $('<button></button>')
     .attr({
-      class: 'btn operator',
+      class: 'operator',
       id: 'sign'
     });
   var $percent = $('<button></button>')
     .attr({
-      class: 'btn operator',
+      class: 'operator',
       id: 'percent'
     });
   $aux.append($clear).append($sign).append($percent).appendTo($calculator);
@@ -36,93 +36,93 @@ $(document).ready(function () {
   // a container for the four operators on the right side clas operator
   var $operators = $('<div></div>')
     .attr({
-      class: 'button-container operators'
+      class: 'container operators'
     });
   // this will contain 5 buttons
   var $division = $('<button></button>')
     .attr({
-      class: 'btn operator',
+      class: 'operator',
       id: 'division'
     });
   var $multiplication = $('<button></button>')
     .attr({
-      class: 'btn operator',
+      class: 'operator',
       id: 'multiplication'
     });
   var $subtraction = $('<button></button>')
     .attr({
-      class: 'btn operator',
+      class: 'operator',
       id: 'subtraction'
     });
   var $addition = $('<button></button>')
     .attr({
-      class: 'btn operator',
+      class: 'operator',
       id: 'addition'
     });
   var $equals = $('<button></button>')
     .attr({
-      class: 'btn operator',
+      class: 'operator',
       id: 'equals'
     });
   $operators.append($division).append($multiplication).append($subtraction).append($addition).append($equals).appendTo($calculator);
 
   // a container for all numbers an decimal
   var $numbers = $('<div></div>')
-    .attr('class', 'button-container numbers');
+    .attr('class', 'container numbers');
   // this will contain 11 buttons
   var $seven = $('<button></button>')
     .attr({
-      class: 'btn number',
+      class: 'number',
       id: 'seven'
     });
   var $eight = $('<button ></button>')
     .attr({
-      class: 'btn number',
+      class: 'number',
       id: 'eight'
     });
   var $nine = $('<button></button>')
     .attr({
-      class: 'btn number',
-      id: 'eight'
+      class: 'number',
+      id: 'nine'
     });
   var $four = $('<button></button>')
     .attr({
-      class: 'btn number',
+      class: 'number',
       id: 'four'
     });
   var $five = $('<button></button>')
     .attr({
-      class: 'btn number',
+      class: 'number',
       id: 'five'
     });
   var $six = $('<button></button>')
     .attr({
-      class: 'btn number',
+      class: 'number',
       id: 'six'
     });
   var $one = $('<button></button>')
     .attr({
-      class: 'btn number',
+      class: 'number',
       id: 'one'
     });
   var $two = $('<button></button>')
     .attr({
-      class: 'btn number',
+      class: 'number',
       id: 'two'
     });
   var $three = $('<button></button>')
     .attr({
-      class: 'btn number',
+      class: 'number',
       id: 'three'
     });
   var $zero = $('<button></button>')
     .attr({
-      class: 'btn number',
+      class: 'number',
       id: 'zero'
     });
   var $decimal = $('<button></button>')
     .attr({
-      class: 'btn number',
+      class: 'number',
       id: 'decimal'
     });
   $numbers.append($seven).append($eight).append($nine).append($four).append($five).append($six).append($one).append($two).append($three).append($zero).append($decimal).appendTo($calculator);
